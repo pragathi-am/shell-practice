5864897ea35e603765d124116727f728f87caecd
prag <pragathiraga@gmail.com>
2026-01-28 18:37:01 +0530

    use variables

---
+++ 03-variables.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# store Ramesh and suresh into 2 variables and use them in code , 
+# for easy maintainance.
+
+person1=Ramesh  #no space btn variable and value
+person2=Suresh
+
+
+echo "$person1 : Hai $person2 , How are you"
+echo "$person2 : Hai $person1 , I am fine, thank you"
+echo "$person1 : good $person2"
