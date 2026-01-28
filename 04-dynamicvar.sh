7be737d445bb505256902bdcc52be1ebd704a116
prag <pragathiraga@gmail.com>
2026-01-28 18:46:46 +0530

    use variables

---
+++ 04-dynamicvar.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# instead of passing Ramesh and Suresh in code, you can send them  , 
+# to code as args dynamically when running script
+
+
+person1=$1  #no space btn variable and value
+person2=$2
+
+
+echo "$person1 : Hai $person2 , How are you"
+echo "$person2 : Hai $person1 , I am fine, thank you"
+echo "$person1 : good $person2"
