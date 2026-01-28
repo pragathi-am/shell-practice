f7995797b534f7bdcd12d0583897bf2a8100eed2
prag <pragathiraga@gmail.com>
2026-01-28 19:38:15 +0530

    use variables

---
+++ 07-timediff.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+#take date command o/p to var1 and print it
+#it can be done by variable=$(command)
+
+var1=$(date +%s)
+echo "script executed at $var1"
+sleep 10
+var2=$(date +%s)
+echo "script stopped at $var2"
+difftime = $(($var2 - $var1))
+echo "script executed for $difftime seconds"
