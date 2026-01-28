4f21f7667a739b052dcc20fab8dd2fc1ebba5eb1
prag <pragathiraga@gmail.com>
2026-01-28 19:22:47 +0530

    use variables

---
+++ 06-printdate.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+#take date command o/p to var1 and print it
+#it can be done by variable=$(command)
+
+var1=$(date)
+echo "script executed at $var1"
+sleep 10
+echo "script stopped at $var2"
