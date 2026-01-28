8bd20fb5d9101798d4905ea26925a98ff00537e2
prag <pragathiraga@gmail.com>
2026-01-28 21:45:55 +0530

    use variables

--- 10-conditions.sh
+++ 10-conditions.sh
@@ -8,6 +8,6 @@ if [$num1 gt 20]; then
    echo "$num1 is greater than 20"
  elif [$num1 eq 20]; then
    echo "$num1 is equal to 20"
- else [$num1 lt 20]; then  
+ else  
    echo "$num1 is less than 20"
 fi
