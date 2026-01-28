f50da30a84e1761b7a82a3c5f9e68641066f356a
prag <pragathiraga@gmail.com>
2026-01-28 21:12:10 +0530

    use variables

---
+++ 09-datatypes.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+num1=100
+num2=20.22
+num3=hello
+
+sum2=$(($num1+$num2))
+sum3=$(($num1+$num3))
+
+echo "sum of $num1 and $num2 is : $sum2"
+echo "sum of $num1 and $num3 is : $sum3"
