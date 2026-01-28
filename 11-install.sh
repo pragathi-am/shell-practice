24c515f787813e4bdf9e486eccfe7173e5bcb71c
prag <pragathiraga@gmail.com>
2026-01-28 22:17:12 +0530

    use variables

--- 11-install.sh
+++ 11-install.sh
@@ -3,7 +3,7 @@
 # to know id of user id . for root user this is always zero
 userid=$(id -u)
 
-if [ $userid -nq 0 ]; then
+if [ $userid -ne 0 ]; then
    echo " please run with sudo user"
    exit 1
 fi
