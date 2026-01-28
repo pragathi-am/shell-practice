2165624a623abffc2a93afaf78cb97571867771c
prag <pragathiraga@gmail.com>
2026-01-28 21:41:08 +0530

    use variables

--- 10-conditions.sh
+++ 10-conditions.sh
@@ -4,10 +4,10 @@ num1=25
 
 # to chck number is greater than 20 or not
 
-if [$num1 gt 20] ; then 
+if [$num1 gt 20]; then 
    echo "$num1 is greater than 20"
- elif [$num1 eq 20] ; then
+ elif [$num1 eq 20]; then
    echo "$num1 is equal to 20"
- else [$num1 lt 20] ; then  
+ else [$num1 lt 20]; then  
    echo "$num1 is less than 20"
 fi
