6ce8223c2193e69dba4fec6ea8815c35cacc0447
prag <pragathiraga@gmail.com>
2026-01-28 19:01:44 +0530

    use variables

--- 05-userinput.sh
+++ 05-userinput.sh
@@ -5,3 +5,10 @@
 echo "what is your name :: "
 read user_name
 echo "person name is : $user_name" 
+
+echo "enter password"
+read -s password
+
+#above -s is used to "not visible the password that typed"
+
+echo " password you typed is $password"
